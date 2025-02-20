c93e099ce8a6820b0f9bab40c78f8008075aa69e
Justin Gardner <justin@rhynorater.com>
2025-02-20 06:02:41 -0500

    3.0.1

---
+++ pojo-accessibility/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pojome/pojo-accessibility',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9c31ef5d593414624a333d51864c9f40d6b250c0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'a49db6f0a5033aef5143295342f1c95521b075ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pojome/pojo-accessibility' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9c31ef5d593414624a333d51864c9f40d6b250c0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
